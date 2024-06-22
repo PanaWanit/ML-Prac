fef58347261bcf2fe229fe14f5f278ebb777c06f
Pana Wanitchollakit <pnaw.wan@gmail.com>
2024-06-22 12:10:47 +0700

    update: seed, output and cuda handler

--- ASR/ASVSpoof/SAMO/main.py
+++ ASR/ASVSpoof/SAMO/main.py
@@ -5,7 +5,7 @@ from omegaconf import DictConfig, OmegaConf
 import logging
 from collections import defaultdict
 
-from samo.utils import train, test
+from samo.utils import setup_seed, output_dir_setup, cuda_checker
 
 @hydra.main(config_path="configs", config_name="cfg", version_base=None)
 def main(cfg: DictConfig) -> None:
@@ -13,6 +13,10 @@ def main(cfg: DictConfig) -> None:
         print(20 *'-', f"{'Config': ^5}", 20 *'-', '\n')
         print(OmegaConf.to_yaml(cfg))
         print(48 * '-')
+    
+    setup_seed(cfg.seed)
+    output_dir_setup(cfg)
+    cuda_checker(cfg)
 
     # feat_model = hydra.utils.instantiate(cfg.model.model)
     
--- ASR/ASVSpoof/SAMO/samo/utils.py
+++ ASR/ASVSpoof/SAMO/samo/utils.py
@@ -1,7 +1,40 @@
 from omegaconf import DictConfig, OmegaConf
+import torch
+import random
+import numpy as np
+import os
+import logging
+import shutil
 
-def train(cfg: DictConfig):
-    raise NotImplemented("Not implemented train function yet")
+def setup_seed(random_seed: int, cudnn_deterministic: bool = True) -> None:
+    torch.manual_seed(random_seed)
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    os.environ["PYTHONHASHSEED"] = str(random_seed)
 
-def test(cfg: DictConfig):
-    raise NotImplemented("Not implemented test function yet")
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(random_seed)
+        torch.backends.cudnn.deterministic = cudnn_deterministic # CUDA convolution determinism
+        torch.backends.cudnn.benchmark = False  # Disabling the benchmarking feature, deterministically select an algorithm
+
+def output_dir_setup(cfg: DictConfig) -> None:
+    if cfg.continue_training:
+        return
+    if os.path.exists(cfg.output_folder):
+        logging.warning(f"{cfg.output_folder} exists!")
+        print(f"Overwrite {cfg.output_folder}")
+    os.makedirs(cfg.output_folder, exist_ok=cfg.overwrite) # ensure that have output_folder exists
+    if cfg.overwrite: # remove then create this directory again
+        shutil.rmtree(cfg.output_folder)
+        os.mkdir(cfg.output_folder)
+        os.makedirs(os.path.join(cfg.output_folder, "checkpoints"))
+    if not os.path.exists(cfg.path_to_database):
+        raise RuntimeError(f"Path {cfg.path_to_database} does not exists!")
+    if not os.path.exists(cfg.path_to_protocol):
+        raise RuntimeError(f"Path {cfg.path_to_database} does not exists!")
+
+def cuda_checker(cfg: DictConfig) -> None:
+    if cfg.device == "cuda" and not torch.cuda.is_available():
+        raise RuntimeError("Cuda is not available on this device!")
+    print(f"using device={cfg.device}")
+    
