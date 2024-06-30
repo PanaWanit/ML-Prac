566372abdb03138701b7f93634d7472d53d332a3
Pana Wanitchollakit <pnaw.wan@gmail.com>
2024-06-30 10:58:17 +0700

    add: tqdm test 2

--- experiment/test_tqdm.py
+++ experiment/test_tqdm.py
@@ -1,7 +1,7 @@
 from time import sleep
-# from tqdm.auto import tqdm
+from tqdm.auto import tqdm
 
-for i in tqdm(range(10)):
-    for j in tqdm(range(10), leave=False):
+for i in tqdm(range(10), desc='epoch', leave=False):
+    for j in tqdm(range(10),  desc='batch', leave=False):
         sleep(.1)
     # print(i, end=' ')
